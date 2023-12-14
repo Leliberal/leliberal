569f4441a5928fd387d82241d005727730c64f06
Leliberal <153089370+Leliberal@users.noreply.github.com>
2023-12-14 17:54:16 +0100

    Add files via upload

---
+++ cookies.js
@@ -0,0 +1,10 @@
+// script.js
+function acceptCookies() {
+    // Add code to set cookies and close the banner
+    document.getElementById('cookieBanner').style.display = 'none';
+  }
+  
+  function refuseCookies() {
+    // Add code to handle when the user refuses cookies
+    document.getElementById('cookieBanner').style.display = 'none';
+  }
